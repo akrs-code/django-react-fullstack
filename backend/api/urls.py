d7da451f25747314e2236294294b12f3ae2ee15a
akrs-code <abdulkhaliqsolaiman@gmail.com>
2026-02-20 23:04:08 +0800

    Configure DRF router and register Country, League, and Characteristic routes

---
+++ backend/api/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('country', CountryViewset, basename='country')
+router.register('league', LeagueViewset, basename='league')
+router.register('characteristic', CharacteristicViewset, basename='characteristic')
+
+urlpatterns = router.urls
