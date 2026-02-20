bc375461b487cd9f3974ef0f67d3ca490427fb38
akrs-code <abdulkhaliqsolaiman@gmail.com>
2026-02-20 22:58:37 +0800

    feat: created components for the navbar and menus

---
+++ frontend/components/ShortMenu.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import { Link, useLocation } from 'react-router';
+
+
+export default function ShortMenu() {
+    const [open, setOpen] = useState(true);
+
+    const handleClick = () => {
+        setOpen(!open);
+    };
+
+    const location = useLocation();
+    const path = location.pathname
+    console.log(path);
+
+    return (
+        <>
+            <List
+                sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+                component="nav"
+                aria-labelledby="nested-list-subheader"
+            >
+                <ListItemButton onClick={handleClick} component={Link} to="/" selected={path === '/'} sx={{display: 'flex', justifyContent: 'center'}}>
+                    <ListItemIcon sx={{display: 'flex', justifyContent: 'center'}}>
+                        <DashboardIcon />
+                    </ListItemIcon>
+                </ListItemButton>
+                <ListItemButton component={Link} to="/create" selected={path === '/create'} sx={{display: 'flex', justifyContent: 'center'}}>
+                    <ListItemIcon sx={{display: 'flex', justifyContent: 'center'}}>
+                        <AddBoxIcon />
+                    </ListItemIcon>
+                </ListItemButton>
+            </List>
+        </>
+    );
+}
