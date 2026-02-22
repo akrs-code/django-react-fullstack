e461605359db96006370891d7298276bd9a50d6c
akrs-code <abdulkhaliqsolaiman@gmail.com>
2026-02-22 23:09:07 +0800

    Refactor DescriptionForm component

--- frontend/components/forms/DescriptionForm.jsx
+++ frontend/components/forms/DescriptionForm.jsx
@@ -1,13 +1,19 @@
 import TextField from '@mui/material/TextField';
 
-export default function DescriptionForm({label, rows=9}) {
+export default function DescriptionForm({ label, rows = 9, value, name, onChange, onBlur, helperText, error }) {
   return (
-        <TextField
-          id="standard-multiline-static"
-          label={label}
-          multiline
-          fullWidth
-          rows={rows}
-        />
+    <TextField
+      id="standard-multiline-static"
+      label={label}
+      multiline
+      fullWidth
+      rows={rows}
+      value={value}
+      name={name}
+      onChange={onChange}
+      onBlur={onBlur}
+        error = {error}
+      helperText={helperText}
+    />
   );
 }
