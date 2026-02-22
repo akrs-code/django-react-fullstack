80fe92f68fe5e4132c8fde44da9ce87330909cf2
akrs-code <abdulkhaliqsolaiman@gmail.com>
2026-02-22 23:09:19 +0800

    Update SelectForm component behavior

--- frontend/components/forms/SelectForm.jsx
+++ frontend/components/forms/SelectForm.jsx
@@ -1,26 +1,23 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import FormHelperText from '@mui/material/FormHelperText';
 
-export default function SelectForm({ label, options }) {
-  const [age, setAge] = React.useState('');
-
-  const handleChange = (event) => {
-    setAge(event.target.value);
-  };
-
+export default function SelectForm({ label, options, value, name, onChange, onBlur, error, helperText }) {
   return (
     <FormControl fullWidth>
       <InputLabel id="demo-simple-select-label">{label}</InputLabel>
       <Select
         labelId="demo-simple-select-label"
         id="demo-simple-select"
-        value={age}
-        label="Age"
-        onChange={handleChange}
+        label={label}
+        value={value}
+        name={name}
+        error={error}
+        helperText={helperText}
+        onChange={onChange}
+        onBlur={onBlur}
       >
         {options.map((option) => {
           return (
@@ -30,6 +27,7 @@ export default function SelectForm({ label, options }) {
           );
         })}
       </Select>
+      <FormHelperText error>{Error}</FormHelperText>
     </FormControl>
   );
 }
