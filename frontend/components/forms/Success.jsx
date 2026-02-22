21e0960868256c978296bbf35088d0fd40bb9a70
akrs-code <abdulkhaliqsolaiman@gmail.com>
2026-02-22 23:11:06 +0800

    Create reusable Success message component

---
+++ frontend/components/forms/Success.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+export default function Message({ messageText, messageColor }) {
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        height: '30px',
+        color: 'white',
+        marginBottom: '20px',
+        padding: '10px',
+        display: 'flex',
+        backgroundColor: messageColor,
+        alignItems: 'center'
+      }}
+    >
+      <Typography>{messageText}</Typography>
+    </Box>
+  );
+}
