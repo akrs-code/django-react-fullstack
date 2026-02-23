e640dd797971ba77d129cae54c2b31983cdee9a0
akrs-code <abdulkhaliqsolaiman@gmail.com>
2026-02-23 21:20:42 +0800

    feat: added Edit and Delete Buttons for actions

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,11 +1,15 @@
 import { useState, useEffect, useMemo } from 'react'
-import { Box, Chip, Typography } from '@mui/material'
+import { Box, Chip, IconButton, Typography } from '@mui/material'
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Link } from 'react-router';
 import CalendarViewMonthIcon from '@mui/icons-material/CalendarViewMonth';
 import { MaterialReactTable } from 'material-react-table'
 import AxiosInstance from '../api/Axios';
 
 const Home = () => {
   const [data, setData] = useState([]);
+
   const GetData = () => {
     AxiosInstance.get('footballclub/').then((res) =>
       setData(res.data)
@@ -51,6 +55,7 @@ const Home = () => {
       }
     ]
   )
+
   return (
     <div>
       <Box
@@ -66,12 +71,25 @@ const Home = () => {
         }}
       >
         <CalendarViewMonthIcon />
-        <Typography sx={{ marginLeft: '15px', fontWeight: 'bold' }} variant='subtitle2'>View all clubs</Typography>
+        <Typography sx={{ marginLeft: '15px', fontWeight: 'bold' }} variant='subtitle2'>
+          View all clubs
+        </Typography>
       </Box>
 
       <MaterialReactTable
         columns={columns}
         data={data}
+        enableRowActions
+        renderRowActions={({ row }) => (
+          <Box sx={{ display: 'flex', flexWrap: 'nowrap', gap: '8px' }}>
+            <IconButton color='primary' component={Link} to={`/edit/${row.original.id}`}>
+              <EditIcon />
+            </IconButton>
+            <IconButton color='error' component={Link} to={`/delete/${row.original.id}`}>
+              <DeleteIcon />
+            </IconButton>
+          </Box>
+        )}
       />
     </div>
   )
