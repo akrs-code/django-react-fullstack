978ce3ecabfdbf77deb0182d5da66d6ae3b1067a
akrs-code <abdulkhaliqsolaiman@gmail.com>
2026-02-23 21:17:16 +0800

    feat: added create, retreive, update, destroy

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,55 +1,75 @@
 from django.shortcuts import render
 from rest_framework import viewsets, permissions
-from .serializers import *
-from .models import *
+from .serializers import * 
+from .models import * 
 from rest_framework.response import Response
 
-# Create your views here.
 class CountryViewset(viewsets.ViewSet):
     permission_classes = [permissions.AllowAny]
     queryset = Country.objects.all()
-    serializers_class = CountrySerializer
+    serializer_class = CountrySerializer
 
     def list(self, request):
         queryset = Country.objects.all()
-        serializers = self.serializers_class(queryset, many=True)
-        return Response(serializers.data)
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
 
 
 class LeagueViewset(viewsets.ViewSet):
     permission_classes = [permissions.AllowAny]
     queryset = League.objects.all()
-    serializers_class = LeagueSerializer
+    serializer_class = LeagueSerializer
 
     def list(self, request):
         queryset = League.objects.all()
-        serializers = self.serializers_class(queryset, many=True)
-        return Response(serializers.data)
-
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
 
 class CharacteristicViewset(viewsets.ViewSet):
     permission_classes = [permissions.AllowAny]
     queryset = Characteristic.objects.all()
-    serializers_class = CharacteristicSerializer
+    serializer_class = CharacteristicSerializer
 
     def list(self, request):
         queryset = Characteristic.objects.all()
-        serializers = self.serializers_class(queryset, many=True)
-        return Response(serializers.data)
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
 
 
-class FootBallClubViewSet(viewsets.ViewSet):
+class FootBallClubViewset(viewsets.ViewSet): 
     permission_classes = [permissions.AllowAny]
+    queryset = FootballClub.objects.all()
     serializer_class = FootballClubSerializer
 
-    def list(self, request):
+    def list(self,request): 
         queryset = FootballClub.objects.all()
         serializer = self.serializer_class(queryset, many=True)
         return Response(serializer.data)
 
-    def create(self, request):
+    def create(self,request): 
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=400)
+        else:
+            return Response(serializer.errors, status=400)
+    
+    def retrieve(self,request,pk=None): 
+        queryset = self.queryset.get(pk=pk)
+        serializer = self.serializer_class(queryset)
+        return Response(serializer.data)
+
+    def update(self,request,pk=None):
+        queryset = self.queryset.get(pk=pk)
+        serializer = self.serializer_class(queryset, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=400)
+
+    def destroy(self, request, pk=None):
+        queryset = self.queryset.get(pk=pk)
+        queryset.delete()
+        return Response(status=204)
+        
